a0d5e60ce6af4232402dc3019d3c9bc87297e5c6
Liza Phan <phantr98@gmail.com>
2024-11-12 17:04:17 +0300

    Fix empty test name

--- test-creator/src/main/java/com/lisa/testcreator/CreateTestNameViewController.java
+++ test-creator/src/main/java/com/lisa/testcreator/CreateTestNameViewController.java
@@ -2,8 +2,8 @@ package com.lisa.testcreator;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 
 public class CreateTestNameViewController {
 
@@ -11,6 +11,8 @@ public class CreateTestNameViewController {
     private Button confirmTestNameButton;
     @FXML
     private TextField testNameTextField;
+    @FXML
+    private Label testNameEmptyLabel;
 
     private TestManager testManager;
 
@@ -20,6 +22,11 @@ public class CreateTestNameViewController {
 
     public void handleConfirmTestNameButtonClicked() {
         String testName = testNameTextField.getText();
-        testManager.setTestToCreateName(testName);
+        if(testName.isEmpty()) {
+            testNameEmptyLabel.setVisible(true);
+        }
+        else {
+            testManager.setTestToCreateName(testName);
+        }
     }
 }
